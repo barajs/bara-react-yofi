7532ca3323d6e0b6c005dd387f41e83920a1dde5
Noah Phom <nampdn@protonmail.com>
2019-05-02 09:43:30 +0000

    chore(App): new design with new API for demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { View } from 'bara-react';
+import { useBarnStream as withBarnStream, useBasicsStream as withBasicsStream, setBarnState } from 'bara';
+import { withBara, useComponentsStream as withComponentsStream, View, Text, useBarnState } from 'bara-react';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { SideBarLayout, SideBar, SideBarItem, SideBarSeparator, SideBarSpace, SideBarContent } from './lib'
+import { SideBarLayout, SideBar, SideBarItem, SideBarSeparator, SideBarSpace, SideBarContent, whenSideBarItemPressed, anySideBarItem, nameOfSideBarItem } from './lib'
+import { useBasicsStream, useBarnStream } from 'bara';
 
 const App: React.FC = () => {
+  const [clicked] = useBarnState('clicked', 'nothing')
   const horizontal = true
   return (
     <View style={{ width: '100%', height: '100%', backgroundColor: '#ccc' }}>
@@ -21,19 +24,37 @@ const App: React.FC = () => {
           <SideBarItem name="light-bulb" iconOcticon="light-bulb" label="Light" />
           <SideBarSpace />
           <SideBarSeparator horizontal={horizontal} />
-          <SideBarItem name="plus" iconOcticon="plus" label="Add" />
+          <SideBarItem name="add" iconOcticon="plus" label="Add" />
           <SideBarItem
             name="settings"
             iconMaterial="settings"
-            label="Settings Long"
+            label="Settings"
           />
         </SideBar>
-        <SideBarContent>
+        <SideBarContent style={{ alignItems: 'center' }}>
           <img src={logo} className="App-logo" alt="logo" />
+          <Text style={{ color: '#f0f', fontSize: 18 }}>Bara React YoFi</Text>
+          <Text style={{ fontSize: 14, margin: 20, padding: 15, borderRadius: 5, backgroundColor: '#c2c2c2' }}>yarn add bara-react-yofi</Text>
+          <Text style={{ color: '#653' }}>You clicked: {clicked} </Text>
         </SideBarContent>
       </SideBarLayout>
     </View>
   );
 }
 
-export default App;
+const baraApp = () => {
+  // Register streams
+  withBasicsStream()
+  withComponentsStream()
+  withBarnStream({ clicked: 'nothing' })
+
+  // Register triggers
+  whenSideBarItemPressed(anySideBarItem)(({ kind, name }) => {
+    setBarnState('clicked', `${kind}/${name}`)
+  })
+  whenSideBarItemPressed(nameOfSideBarItem('add'))(() => {
+    prompt("Please enter your new item", "Item name")
+  })
+}
+
+export default withBara(App, baraApp);
